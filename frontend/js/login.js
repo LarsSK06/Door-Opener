b83d35ef037d2889d520c086d6e0ad5dc1c9fbe9
ApenJulius <34554076+ApenJulius@users.noreply.github.com>
2023-09-25 15:01:01 +0200

    Update login.js
    Readded drop down

--- frontend/js/login.js
+++ frontend/js/login.js
@@ -2,6 +2,20 @@
 const ip = "http://127.0.0.1:80";
 const port = 4000;
 window.addEventListener("load", () => {
+    const header = document.querySelector("#header");
+        header.querySelectorAll(".column").forEach(column => {
+            const clicker = column.querySelector("#button").querySelector("#text");
+            clicker.addEventListener("click", () => {
+                if(column.hasAttribute("selected")){
+                    column.removeAttribute("selected");
+                }
+                else if(header.querySelector(".column[selected]")){
+                    header.querySelector(".column[selected]").removeAttribute("selected");
+                    column.setAttribute("selected", "");
+                }
+                else column.setAttribute("selected", "");
+            });
+        });
     document.querySelector("#login-form").addEventListener("submit", (event) => {
         event.preventDefault()
         fetch(ip + "/login", {
